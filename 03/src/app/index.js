3f3014917d8f83afe1f5461856986633b2281f31
Dmitrii <dima_sh84@mail.ru>
2017-01-14 21:42:52 +0200

    passing data with props

---
+++ 03/src/app/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    render() {
+        var user = {
+            name: "Tania",
+            hobbies: ["Sports", "Reading"]
+        };
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sx-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sx-10 col-xs-offset-1">
+                        <Home name={"Dima"} age={32} user={user}>
+                            <p>This is a paragraph</p>
+                        </Home>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
