3249319fddf2ac0e4f22a01dbf364e5a0b1108d1
Dmitrii <dima_sh84@mail.ru>
2017-01-14 21:54:32 +0200

    events

---
+++ 04/src/app/components/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    onMakeOlder() {
+        this.age += 3;
+        console.log(this.age);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>In a new Component</p>
+                <p>Your name is {this.props.name}, your age is {this.age}</p>
+                <hr/>
+                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older</button>
+            </div>
+        );
+    }
+}
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number,
+}
