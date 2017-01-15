4e093535a68e777d58c24b078c2d934e50b6e1c9
Dmitrii <dima_sh84@mail.ru>
2017-01-15 21:24:14 +0200

    passing data between parent and child components

---
+++ 06/src/app/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
+    onGreet() {
+        alert("Hello");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sx-10 col-xs-offset-1">
+                        <Header homeLink={this.state.homeLink} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sx-10 col-xs-offset-1">
+                        <Home
+                            name={"Dima"}
+                            initialAge={32}
+                            greet={this.onGreet}
+                            changeLink={this.onChangeLinkName.bind(this)}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
+{/*greet={onGreet}*/}
